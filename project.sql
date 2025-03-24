e7ef485ee2cf6779f37316ec177e3e403aa16958
Jayden De Souza <113254213+JAE-exe@users.noreply.github.com>
2025-03-24 23:05:22 +0530

    Update project.sql

--- project.sql
+++ project.sql
@@ -1,1 +1,28 @@
+/*
 
+Cleaning Data in SQL Queries
+
+*/
+
+SELECT SaleDate,CONVERT(DATE,SaleDate)
+FROM NHD;
+
+
+UPDATE NHD
+SET SaleDate=CONVERT(DATE,SaleDate);
+
+
+SELECT a.ParcelID,a.PropertyAddress,b.ParcelID,b.PropertyAddress,ISNULL(a.PropertyAddress,b.PropertyAddress)
+FROM NHD a
+JOIN NHD b
+	ON a.ParcelID = b.ParcelID
+	AND a.[UniqueID]<>B.[UniqueID]
+WHERE a.PropertyAddress IS NULL;
+
+UPDATE a
+SET PropertyAddress= ISNULL(a.PropertyAddress,b.PropertyAddress)
+FROM NHD a
+JOIN NHD b
+	ON a.ParcelID = b.ParcelID
+	AND a.[UniqueID]<>B.[UniqueID]
+WHERE a.PropertyAddress IS NULL;
