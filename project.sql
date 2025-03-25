658e9a87ac547f87fe11a138a929afc2f2fcbfb1
Jayden De Souza <113254213+JAE-exe@users.noreply.github.com>
2025-03-25 09:39:54 +0530

    Update project.sql

--- project.sql
+++ project.sql
@@ -26,3 +26,26 @@ JOIN NHD b
 	ON a.ParcelID = b.ParcelID
 	AND a.[UniqueID]<>B.[UniqueID]
 WHERE a.PropertyAddress IS NULL;
+
+
+SELECT PropertyAddress FROM NHD;
+
+
+SELECT SUBSTRING(PropertyAddress,1,CHARINDEX(',',PropertyAddress)-1) AS Address
+FROM NHD;
+
+SELECT SUBSTRING(PropertyAddress,1,CHARINDEX(',',PropertyAddress)-1) AS Address,
+SUBSTRING(PropertyAddress,CHARINDEX(',',PropertyAddress)+1,LEN(PropertyAddress)) AS Address
+FROM NHD;
+
+ALTER TABLE NHD
+ADD PropertyStreetAddress NVARCHAR(255);
+
+UPDATE NHD
+SET PropertyStreetAddress = SUBSTRING(PropertyAddress,1,CHARINDEX(',',PropertyAddress)-1);
+
+ALTER TABLE NHD
+ADD PropertyCityAddress NVARCHAR(255);
+
+UPDATE NHD
+SET PropertyCityAddress = SUBSTRING(PropertyAddress,CHARINDEX(',',PropertyAddress)+1,LEN(PropertyAddress));
